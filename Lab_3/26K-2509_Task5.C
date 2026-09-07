65b18111221b271fa4c7e9f49c3f370beb0e31a2
aarishifham7-com <aarishifham7@gmail.com>
2026-09-07 14:35:27 +0500

    Update student information in Task5.C

--- Lab_3/26K-2509_Task5.C
+++ Lab_3/26K-2509_Task5.C
@@ -1,10 +1,7 @@
 #include <stdio.h>
 int main(){
 
-    printf("Name     \tAge\tGrade\n");
-    printf("Hamza    \t19\tD\n");
-    printf("Zunoorain\t20\tA1\n");
-    printf("Umer     \t20\tA");
+    printf("Name \tAge\tGrade\nAarish\t19\tD\nMaha\t20\tA1\nFizza\t20\tA");
 
     return 0;
 }
